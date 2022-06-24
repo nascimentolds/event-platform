3e040df3195b275d37aff6aad10a6d7f8707eddb
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2022-06-24 00:53:38 -0300

    Rotas criadas

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,23 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Video />
+        {slug ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className="flex-1">
+            <div className="bg-black flex justify-center">
+              <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+            </div>
+          </div>
+        )}
         <Sidebar />
       </main>
     </div>
