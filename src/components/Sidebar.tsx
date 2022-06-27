77d8a3bbc2a4da3b2c54e91e069ee096d380eca6
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2022-06-26 23:27:49 -0300

    Incluido Codegen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      availableAt
-      title
-      slug
-    }
-  }
-`;
-
-interface getLessonsQueryResponse {
-  lessons: {
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string;
-    lessonType: "live" | "lesson";
-  }[];
-}
-
 export function Sidebar() {
-  const { data } = useQuery<getLessonsQueryResponse>(GET_LESSONS_QUERY);
+  const { data } = useGetLessonsQuery()
 
   console.log(data);
 
